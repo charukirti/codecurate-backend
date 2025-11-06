2195849fbbe596e738898945505584c218b5914c
charukirti <charukirticc112002@gmail.com>
2025-11-06 14:40:33 +0530

    feat(db): add users and resource schema

---
+++ src/db/schema/resources.ts
@@ -0,0 +1,39 @@
+import {
+  integer,
+  jsonb,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const typeEnum = pgEnum("type", ["video", "playlist"]);
+
+export const resources = pgTable("resources", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  title: varchar("video_title", { length: 255 }).notNull(),
+  description: text("video_description").notNull(),
+  type: typeEnum("type").default("video").notNull(),
+  channelId: varchar("channel_id", { length: 100 }),
+  channelName: varchar("channel_name", { length: 255 }).notNull(),
+  publishedAt: timestamp("published_at").notNull(),
+  thumbnails: jsonb("thumbnails").notNull(),
+  durationIso: varchar("duration_iso", { length: 20 }).notNull(),
+  durationSeconds: integer("duration_seconds").notNull(),
+  viewCount: integer("view_count").notNull(),
+  likeCount: integer("like_count").notNull(),
+  videoLang: varchar("video_lang").notNull(),
+  codeLang: varchar("code_lang").notNull(),
+  topic: varchar("video_topic", { length: 100 }).notNull(),
+  playlistId: varchar("playlist_id", { length: 100 }),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+// type for data coming out of database after querying
+export type Resource = typeof resources.$inferSelect;
+
+// type for data going inside database
+export type NewResource = typeof resources.$inferInsert;
---
+++ src/db/schema/users.ts
@@ -0,0 +1,24 @@
+import {
+  pgTable,
+  varchar,
+  uuid,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  username: varchar("username", { length: 100 }).notNull().unique(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  isAdmin: boolean("is_admin").default(false).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+// type for data coming out of database after querying
+export type User = typeof users.$inferSelect
+
+// type for data going inside database
+export type NewUser = typeof users.$inferInsert
